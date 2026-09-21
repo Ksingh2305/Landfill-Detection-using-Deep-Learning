1eef27b7b3284c9dffbd26f350d2864297f61953
Ksingh2305 <ksingh3_be23@thapar.edu>
2026-09-21 14:19:09 +0530

    Delete __init__.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-"""Landfill Detection from Satellite Imagery.
-
-A reproducible, end-to-end computer-vision pipeline:
-download -> preprocess -> train -> evaluate -> explain -> serve.
-"""
-
-__version__ = "1.0.0"
